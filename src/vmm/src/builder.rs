7d34794f26d9c0c63dfba43b044e702ff3de05c5
James Curtis <jxcurtis@amazon.co.uk>
2024-07-16 14:23:52 +0000

    Update unit tests
    MMIO tests:
    The addresses and IRQs of devices created in `test_attach_...` tests are
    now slightly different due to the implementation of the CpuContainer.
    The IRQs are have been increased by 2, and the address has been
    increased by 0x1000.
    
    This occurs because the CPUContainer has 2 IRQs and one MMIO device,
    which takes up 4K.
    
    ACPI Table tests:
    Test `test_write_acpi_tables_small_memory` was failing due to additional
    pages being allocated to CpuContainer. This led to the corner case not
    being reproduced. Values have been changed in order to properly
    reproduce this corner case.
    
    Signed-off-by: James Curtis <jxcurtis@amazon.co.uk>

--- src/vmm/src/builder.rs
+++ src/vmm/src/builder.rs
@@ -1630,8 +1630,8 @@ pub mod tests {
             #[cfg(any(target_arch = "x86", target_arch = "x86_64"))]
             assert!(cmdline_contains(
                 &cmdline,
-                "virtio_mmio.device=4K@0xd0000000:5 virtio_mmio.device=4K@0xd0001000:6 \
-                 virtio_mmio.device=4K@0xd0002000:7"
+                "virtio_mmio.device=4K@0xd0001000:7 virtio_mmio.device=4K@0xd0002000:8 \
+                 virtio_mmio.device=4K@0xd0003000:9"
             ));
         }
 
@@ -1726,7 +1726,7 @@ pub mod tests {
         #[cfg(any(target_arch = "x86", target_arch = "x86_64"))]
         assert!(cmdline_contains(
             &cmdline,
-            "virtio_mmio.device=4K@0xd0000000:5"
+            "virtio_mmio.device=4K@0xd0001000:7"
         ));
     }
 
@@ -1743,7 +1743,7 @@ pub mod tests {
         #[cfg(any(target_arch = "x86", target_arch = "x86_64"))]
         assert!(cmdline_contains(
             &cmdline,
-            "virtio_mmio.device=4K@0xd0000000:5"
+            "virtio_mmio.device=4K@0xd0001000:7"
         ));
     }
 
@@ -1762,7 +1762,7 @@ pub mod tests {
         #[cfg(any(target_arch = "x86", target_arch = "x86_64"))]
         assert!(cmdline_contains(
             &cmdline,
-            "virtio_mmio.device=4K@0xd0000000:5"
+            "virtio_mmio.device=4K@0xd0001000:7"
         ));
     }
 }
